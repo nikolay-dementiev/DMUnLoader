1efd4fbbe0118abc1498a99c5ce89f20f95d2b39
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-08-16 18:06:51 +0200

    renamed and remake snapshots for default case (VerifyDefaultInitialization)

---
+++ Tests/DMUnLoaderTests/SnapshotTesting/Helpers/SnapshotStrategies.swift
@@ -0,0 +1,55 @@
+//
+//  DMUnLoader
+//
+//  Created by Mykola Dementiev
+//
+
+import SwiftUI
+import PreviewSnapshotsTesting
+
+#if os(iOS) || os(tvOS)
+let frameworkName = "UIKit"
+
+extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
+    static var testStrategyiPhone13Light: Self {
+        .image(
+            layout: .device(config: .iPhone13(.portrait)),
+            traits: UITraitCollection(userInterfaceStyle: .light)
+        )
+    }
+    
+    static var testStrategyiPhone13Dark: Self {
+        .image(
+            layout: .device(config: .iPhone13(.portrait)),
+            traits: UITraitCollection(userInterfaceStyle: .dark)
+        )
+    }
+}
+
+#elseif os(macOS)
+let frameworkName = "AppKit"
+
+@MainActor
+extension Snapshotting where Value: SwiftUI.View, Format == NSImage {
+    static var testStrategyiPhone13Light: Self {
+        getStrategy(appearance: .vibrantLight)
+    }
+    
+    static var testStrategyiPhone13Dark: Self {
+        getStrategy(appearance: .vibrantDark)
+    }
+    
+    private static func getStrategy(appearance appearanceName: NSAppearance.Name) -> Self {
+        Snapshotting<NSView, NSImage>
+            .image(size: .init(width: 800, height: 400))
+            .pullback { view in
+            let view = NSHostingView(rootView: view)
+            view.appearance = NSAppearance(named: appearanceName)
+        
+            view.wantsLayer = true
+            view.layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+            return view
+        }
+    }
+}
+#endif
