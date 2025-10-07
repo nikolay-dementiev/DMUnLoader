512a3e8032ab0d63e66ea6959bd5e5214ee5a86f
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-10-07 12:25:24 +0200

    got rid an extra function

--- Sources/DMUnLoader/Helpers/HUD/FS/FSSceneDelegateHelper.swift
+++ Sources/DMUnLoader/Helpers/HUD/FS/FSSceneDelegateHelper.swift
@@ -4,18 +4,10 @@
 import UIKit
 
 public protocol FSSceneDelegateHelper {
-    @MainActor
-    static func makeLoadingManager<LM>() -> LM where LM: DMLoadingManagerProtocol
-    
     static func makeUIKitRootViewHierarhy<LM: DMLoadingManagerProtocol>(loadingManager: LM) -> UIViewController
 }
 
 extension FSSceneDelegateHelper {
-    @MainActor
-    public static func makeLoadingManager<LM: DMLoadingManagerProtocol>() -> LM {
-        LM()
-    }
-    
     static func makeUIKitRootViewHierarhy<LM: DMLoadingManagerProtocol>(loadingManager: LM) -> UIViewController {
         
         UIViewController(nibName: nil, bundle: nil)
