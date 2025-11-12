86a7754fd68366093ccce95f453b21363d633f6a
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-11-12 19:22:10 +0100

    move DMSuccessViewTDD in production. Swap it with DMSuccessView

--- Tests/DMUnLoaderTests/Views/Package views/DMSuccessViewTests.swift
+++ Tests/DMUnLoaderTests/Views/Package views/DMSuccessViewTests.swift
@@ -8,58 +8,284 @@ import XCTest
 @testable import DMUnLoader
 import SwiftUI
 import ViewInspector
+import SnapshotTesting
 
+@MainActor
 final class DMSuccessViewTests: XCTestCase {
     
-    // MARK: Initialization Tests
+    override func invokeTest() {
+        withSnapshotTesting(
+            record: .missing,
+            diffTool: .ksdiff
+        ) {
+            super.invokeTest()
+        }
+    }
+
+    // MARK: Scenario 1: Verify Default Initialization
     
-    @MainActor
-    func testInitialization() {
-        let settingsProvider = MockDMSuccessViewSettings()
-        let successObject = MockDMLoadableTypeSuccess()
-        let view = DMSuccessView(settings: settingsProvider,
-                                 assosiatedObject: successObject)
+    func testDefaultInitialization() {
+        let settings = DMSuccessDefaultViewSettings()
+        let sut = makeSUT(settings: settings)
         
-        XCTAssertNotNil(view.settingsProvider,
+        XCTAssertNotNil(sut.settingsProvider,
                         "The settingsProvider should be initialized")
-        XCTAssertNotNil(view.assosiatedObject,
-                        "The associated object should be initialized")
     }
     
-    // MARK: Rendering Tests
-    
-    @MainActor
-    func testRendersContainerView() throws {
-        let settingsProvider = MockDMSuccessViewSettings()
-        let successObject = MockDMLoadableTypeSuccess()
-        let view = DMSuccessView(settings: settingsProvider,
-                                 assosiatedObject: successObject)
+    func testImageRenderedCorrectlyDuringDefaultInitialization() throws {
+        let settings = DMSuccessDefaultViewSettings()
+        let sut = makeSUT(settings: settings)
         
-        let containerView = try? view
-            .inspect()
-            .find(viewWithTag: DMSuccessViewOwnSettings.containerViewTag)
-            .vStack()
-        
-        XCTAssertNotNil(containerView,
-                        "The container view should be rendered")
-    }
-    
-    @MainActor
-    func testRendersImageView() throws {
-        let settingsProvider = MockDMSuccessViewSettings()
-        let successObject = MockDMLoadableTypeSuccess()
-        let view = DMSuccessView(settings: settingsProvider,
-                                 assosiatedObject: successObject)
-        
-        let imageView = try? view
+        let image = try sut
             .inspect()
             .find(viewWithTag: DMSuccessViewOwnSettings.imageTag)
             .image()
-        XCTAssertNotNil(imageView,
-                        "The ImageView should be rendered")
+
+        try sutImageNameConfirmToExpectedImage(
+            sutImage: image,
+            expectedImage: settings.successImageProperties.image,
+            expectedImageName: "checkmark.circle.fill"
+        )
+    }
+    
+    func testTextRenderedCorrectlyDuringDefaultInitialization() throws {
+        let settings = DMSuccessDefaultViewSettings()
+        let sut = makeSUT(settings: settings)
         
-        let fixedImageFrame = try? imageView?.fixedFrame()
-        let successImageProperties = settingsProvider.successImageProperties
+        let text = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.textTag)
+            .text()
+        
+        XCTAssertNotNil(text,
+                        "The TextView should be rendered")
+
+        let textFormSUT = try? text.string()
+        XCTAssertEqual(textFormSUT,
+                       settings.successTextProperties.text,
+                       "The default success text should match the settings")
+        XCTAssertEqual(textFormSUT,
+                       "Success!",
+                       "The default success text should match the settings")
+    }
+    
+    // MARK: Scenario 2: Verify Success Image Behavior
+    
+    func testCustomSuccessImageRendering() throws {
+        let settings = DMSuccessDefaultViewSettings(
+            successImageProperties: .init(
+                image: Image(systemName: "star.fill"),
+                frame: CustomViewSize(width: 60, height: 60),
+                foregroundColor: .yellow
+            )
+        )
+        let sut = makeSUT(settings: settings)
+        
+        let image = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.imageTag)
+            .image()
+        
+        try sutImageNameConfirmToExpectedImage(
+            sutImage: image,
+            expectedImage: settings.successImageProperties.image,
+            expectedImageName: "star.fill"
+        )
+        
+        try sutImageForegroundColorConfirmToExpectedColor(
+            sutImage: image,
+            expectedForegroundColorInSettings: settings.successImageProperties.foregroundColor,
+            expectedForegroundColor: .yellow
+        )
+        
+        try sutImageFrameSizeConfirmToExpectedsize(
+            sutImage: image,
+            expectedSizeInSettings: settings.successImageProperties.frame,
+            expectedSize: CustomViewSize(width: 60, height: 60)
+        )
+        
+        try sutImageResizableParametersConfirmToExpectedDefault(sutImage: image)
+    }
+    
+    private func sutImageNameConfirmToExpectedImage(
+        sutImage: InspectableView<ViewType.Image>,
+        expectedImage: Image,
+        expectedImageName: String,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
+        let imageFormSUT = try sutImage.actualImage().name()
+        
+        XCTAssertEqual(
+            imageFormSUT,
+            try expectedImage.name(),
+            "The custom success image should match the settings"
+        )
+        XCTAssertEqual(
+            imageFormSUT,
+            expectedImageName,
+            "The custom success image should be '\(expectedImageName)'"
+        )
+    }
+    
+    private func sutImageForegroundColorConfirmToExpectedColor(
+        sutImage: InspectableView<ViewType.Image>,
+        expectedForegroundColorInSettings: Color?,
+        expectedForegroundColor: Color,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
+        let imageForegroundColorFormSUT = try sutImage.foregroundColor()
+        
+        XCTAssertEqual(
+            imageForegroundColorFormSUT,
+            expectedForegroundColorInSettings,
+            "Success image foreground color should match the settings"
+        )
+        XCTAssertEqual(
+            imageForegroundColorFormSUT,
+            expectedForegroundColor,
+            "Success image foreground color should be \(expectedForegroundColor)"
+        )
+    }
+    
+    private func sutImageFrameSizeConfirmToExpectedsize(
+        sutImage: InspectableView<ViewType.Image>,
+        expectedSizeInSettings: CustomViewSize,
+        expectedSize: CustomViewSize,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
+        let fixedImageFrame = try sutImage.fixedFrame()
+        
+        [
+            "Settings size": expectedSizeInSettings,
+            "Expected size": expectedSize
+        ]
+            .forEach { key, value in
+                XCTAssertEqual(fixedImageFrame.width,
+                               value.width,
+                               "The ImageView `\(key)` frame should have the correct frame width")
+                XCTAssertEqual(fixedImageFrame.height,
+                               value.height,
+                               "The ImageView `\(key)` height should have the correct frame height")
+                XCTAssertEqual(fixedImageFrame.alignment,
+                               value.alignment,
+                               "The ImageView `\(key)` frame should have the correct frame alignment")
+            }
+    }
+    
+    private func sutImageResizableParametersConfirmToExpectedDefault(
+        sutImage: InspectableView<ViewType.Image>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
+        let resizableParameters = try sutImage.actualImage().resizableParameters()
+        XCTAssertEqual(
+            resizableParameters.capInsets,
+            EdgeInsets(),
+            "The resizable image capInsets should be equal to EdgeInsets()"
+        )
+        XCTAssertEqual(
+            resizableParameters.resizingMode,
+            .stretch,
+            "The resizable image resizingMode should be .stretch"
+        )
+    }
+    
+    // MARK: Scenario 3: Verify Success Text Behavior
+    
+    func testCustomSuccessTextRendering() throws {
+        let expectedText = "Operation Completed!"
+        let expectedForegroundColor = Color.green
+        let settings = DMSuccessDefaultViewSettings(
+            successTextProperties: .init(
+                text: expectedText,
+                foregroundColor: expectedForegroundColor
+            )
+        )
+        let sut = makeSUT(settings: settings)
+        
+        let text = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.textTag)
+            .text()
+        
+        XCTAssertEqual(
+            try text.string(),
+            expectedText,
+            "The custom success text should match the `\(expectedText)`"
+        )
+        
+        XCTAssertEqual(
+            try text.attributes().foregroundColor(),
+            expectedForegroundColor,
+            "The custom success text foreground color should be `\(expectedForegroundColor)`"
+        )
+    }
+    
+    func testCustomSuccessTextRederedAssosiatedObject() throws {
+        let expectedText = "Operation Completed!"
+        let sut = makeSUT(
+            settings: DMSuccessDefaultViewSettings(),
+            assosiatedObject: MockDMLoadableTypeSuccess(description: expectedText)
+        )
+        
+        let text = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.textTag)
+            .text()
+        
+        XCTAssertEqual(
+            try text.string(),
+            expectedText,
+            "The custom success text should match the `\(expectedText)`"
+        )
+    }
+    
+    // MARK: Scenario 4: Verify Layout and Alignment
+    
+    func testThatTheImageVerticallyAligned() throws {
+        let sut = makeSUT(settings: DMSuccessDefaultViewSettings())
+        
+        let image = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.imageTag)
+            .image()
+        
+        XCTAssertEqual(
+            try image.fixedAlignment(),
+            .center,
+            "The ImageView should be center aligned"
+        )
+    }
+    
+    func testThatTheTextVerticallyAligned() throws {
+        let sut = makeSUT(settings: DMSuccessDefaultViewSettings())
+        
+        let text = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.textTag)
+            .text()
+        
+        XCTAssertEqual(
+            try text.fixedAlignment(),
+            .center,
+            "The TextView should be center aligned"
+        )
+    }
+    
+    func testRendersImageFrame() throws {
+        let settings = DMSuccessDefaultViewSettings()
+        let sut = makeSUT(settings: settings)
+        
+        let imageView = try sut
+            .inspect()
+            .find(viewWithTag: DMSuccessViewOwnSettings.imageTag)
+            .image()
+        
+        let fixedImageFrame = try? imageView.fixedFrame()
+        let successImageProperties = settings.successImageProperties
         
         XCTAssertEqual(fixedImageFrame?.width,
                        successImageProperties.frame.width,
@@ -70,53 +296,113 @@ final class DMSuccessViewTests: XCTestCase {
         XCTAssertEqual(fixedImageFrame?.alignment,
                        successImageProperties.frame.alignment,
                        "The ImageView frame should have the correct frame alignment")
-        
-        XCTAssertEqual(try? imageView?.foregroundColor(),
-                       successImageProperties.foregroundColor,
-                       "The ImageView should have the correct foreground color")
     }
     
-    @MainActor
-    func testRendersTextFromAssociatedObject() throws {
-        let settingsProvider = MockDMSuccessViewSettings()
-        let successObject = MockDMLoadableTypeSuccess(description: "Custom Success Message")
-        let view = DMSuccessView(settings: settingsProvider,
-                                 assosiatedObject: successObject)
+    func testSpacingBetweenTheImageAndTextIsConsistent() throws {
+        let expectedSpacing: CGFloat = 15.0
+        let settings = DMSuccessDefaultViewSettings(
+            spacingBetweenElements: expectedSpacing
+        )
+        let sut = makeSUT(settings: settings)
         
-        let textView = try? view
+        let containerView = try sut
             .inspect()
-            .find(viewWithTag: DMSuccessViewOwnSettings.textTag)
-            .text()
-        XCTAssertNotNil(textView,
-                        "The Text view should be rendered")
+            .find(viewWithTag: DMSuccessViewOwnSettings.containerViewTag)
+            .vStack()
         
-        XCTAssertEqual(try? textView?.string(),
-                       successObject.description,
-                       "The Text view should display the custom success message from the associated object")
-        XCTAssertEqual(try? textView?.attributes().foregroundColor(),
-                       settingsProvider.successTextProperties.foregroundColor,
-                       "The Text view should have the correct foreground color")
+        XCTAssertEqual(
+            try containerView.spacing(),
+            expectedSpacing,
+            "The spacing between image and text should be `\(expectedSpacing)`"
+        )
     }
     
-    @MainActor
-    func testRendersTextFromSettingsProvider() throws {
-        let settingsProvider = MockDMSuccessViewSettings()
-        let successObject: MockDMLoadableTypeSuccess? = nil
-        let view = DMSuccessView(settings: settingsProvider,
-                                 assosiatedObject: successObject)
+    // MARK: Scenario 5: Verify Snapshot Testing
+    
+    func testSnapshotDefaultSuccessView() async throws {
+        let settings = DMSuccessDefaultViewSettings()
+        let sut = makeSUTWithContainer(settings: settings)
         
-        let textView = try view
-            .inspect()
-            .find(viewWithTag: DMSuccessViewOwnSettings.textTag)
-            .text()
-        XCTAssertNotNil(textView,
-                        "The Text view should be rendered")
+        assertSnapshot(
+            of: sut,
+            as: .image(
+                layout: .device(config: .iPhone13Pro),
+                traits: .init(userInterfaceStyle: .dark)
+            ),
+            named: "iPhone13Pro-dark"
+        )
         
-        XCTAssertEqual(try? textView.string(),
-                       settingsProvider.successTextProperties.text,
-                       "The Text view should display the default success text from the settings provider")
-        XCTAssertEqual(try? textView.attributes().foregroundColor(),
-                       settingsProvider.successTextProperties.foregroundColor,
-                       "The Text view should have the correct foreground color")
+        assertSnapshot(
+            of: sut,
+            as: .image(
+                layout: .device(config: .iPhone13Pro),
+                traits: .init(userInterfaceStyle: .light)
+            ),
+            named: "iPhone13Pro-light"
+        )
+    }
+    
+    func testSnapshotCustomSuccessView() async throws {
+        let settings = DMSuccessDefaultViewSettings(
+            successImageProperties: .init(
+                image: Image(systemName: "star.fill"),
+                frame: CustomViewSize(width: 61, height: 70),
+                foregroundColor: .cyan
+            ),
+            successTextProperties: .init(
+                text: "Operation Completed!",
+                foregroundColor: .pink
+            ),
+            spacingBetweenElements: 22.2
+        )
+        
+        let sut = makeSUTWithContainer(
+            settings: settings,
+            assosiatedObject: MockDMLoadableTypeSuccess(description: "All tasks finished successfully.")
+        )
+        
+        assertSnapshot(
+            of: sut,
+            as: .image(
+                layout: .device(config: .iPhone13Pro),
+                traits: .init(userInterfaceStyle: .dark)
+            ),
+            named: "iPhone13Pro-dark"
+        )
+        
+        assertSnapshot(
+            of: sut,
+            as: .image(
+                layout: .device(config: .iPhone13Pro),
+                traits: .init(userInterfaceStyle: .light)
+            ),
+            named: "iPhone13Pro-light"
+        )
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        settings: DMSuccessViewSettings,
+        assosiatedObject: DMLoadableTypeSuccess? = nil
+    ) -> DMSuccessView {
+        let sut = DMSuccessView(
+            settings: settings,
+            assosiatedObject: assosiatedObject
+        )
+        
+        return sut
+    }
+    
+    private func makeSUTWithContainer(
+        settings: DMSuccessViewSettings,
+        assosiatedObject: DMLoadableTypeSuccess? = nil
+    ) -> LoadingViewContainer<DMSuccessView> {
+        LoadingViewContainer {
+            DMSuccessView(
+                settings: settings,
+                assosiatedObject: assosiatedObject
+            )
+        }
     }
 }
